819adbc8110c610240f6dad076273b6204a7bfe9
Rex Danger <brian.quinlan@darkwolfsolutions.com>
2019-03-29 21:27:08 +0000

    Upload main.tf

---
+++ main.tf
@@ -0,0 +1,75 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = "${file("serviceaccount.json")}"
+ project     = "brew-wolf"
+ region      = "us-east4"
+}
+
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+    byte_length = 8
+}
+
+// A Google Cloud Engine instances
+resource "google_compute_instance" "instance-" {
+    machine_type = "f1-micro"
+    zone         = "us-east4-b"
+    name         = "web-${count.index}"
+    count        = 2
+
+    boot_disk {
+        initialize_params {
+            image = "centos-7-v20190326"
+        }
+    }
+
+    // Make sure docker is installed on all new instances for later steps
+    metadata_startup_script = "sudo yum update -y; sudo yum install -y docker; sudo systemctl enable docker; sudo systemctl start docker; sudo docker run -d -p 80:80 us.gcr.io/brew-wolf/app:master; echo \"Deployed $${image_message}\""
+
+    network_interface {
+        network = "default"
+
+        access_config {
+            // Include this section to give the VM an external ip address
+        }
+    }
+
+
+}
+
+resource "google_compute_instance_group" "brew-resources" {
+    name        = "brew-wolf-instances"
+    description = "Brew Wolf instance group"
+    zone        = "us-east4-b"
+
+    instances = ["${google_compute_instance.instance-.*.self_link}"]
+    
+    named_port {
+        name = "http",
+        port = "80",
+    }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "brew-wolf-firewall"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "22"]
+  }
+
+  source_tags = ["web"]
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "brew-wolf-states"
+    prefix = "prod"
+    credentials = "serviceaccount.json"
+  }
+}
