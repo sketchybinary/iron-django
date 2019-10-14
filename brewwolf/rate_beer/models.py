4199a22436ee8575f7a57869b64aa4d7f5d1588f
will.kline <will.kline@wkline.darkwolf.io>
2019-10-14 16:57:00 -0400

    Move the id field to be a uuid

--- brewwolf/rate_beer/models.py
+++ brewwolf/rate_beer/models.py
@@ -1,7 +1,11 @@
-from django.db import models
+import uuid
+
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
 
 class Beer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     brewery = models.CharField(max_length=200)
     beer_type = models.CharField(max_length=200)
@@ -9,12 +13,13 @@ class Beer(models.Model):
     @property
     def average_rating(self):
         ratings = Rating.objects.filter(beer=self.id)
-        if  len(ratings) == 0:
+        if len(ratings) == 0:
             return "NA"
         else:
             return sum([r.rating for r in ratings]) / len(ratings)        
 
 class Rating(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
     user = models.CharField(max_length=200)
     rating = models.IntegerField(default=5, validators=[MaxValueValidator(5), MinValueValidator(1)])
