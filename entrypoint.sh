7cae9aa92788442217e96666bbddffecd13741b5
will.kline <will.kline@wkline.darkwolf.io>
2019-10-14 16:51:48 -0400

    If running without external DB, we should migrate our internal sqlite

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# If we don't have a database, migrate one
+if [ -z "$BREW_DB" ]; then
+    python manage.py migrate
+fi
+
 /usr/local/bin/gunicorn --bind 0.0.0.0:80 \
                         --error-logfile - \
                         brewwolf.wsgi:application
