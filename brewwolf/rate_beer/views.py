5a70674fa36fd892d566ca329e53f3a193896728
will.kline <will.kline@wkline.darkwolf.io>
2019-10-16 08:15:25 -0400

    remove unused imports

--- brewwolf/rate_beer/views.py
+++ brewwolf/rate_beer/views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from rate_beer.models import Beer, Rating
 from rate_beer.serializers import BeerSerializer, RatingSerializer
